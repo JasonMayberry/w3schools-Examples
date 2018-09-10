5e877566bc1d03148210eefe771c06d2cabca941
Jason Mayberry <42257169+JasonMayberry@users.noreply.github.com>
2018-09-09 19:46:14 -0500

    Create demo_db_connection.js

---
+++ Node.js-MySQL/demo_db_connection.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+// This script not working?
+// Check out the PDF in this repository
+// Node.js-MySQL-createConnection.pdf
